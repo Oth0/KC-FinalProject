a05a93189a692e084d9ef20f546dd8bedf7dc7c8
Oth0 <109856688+Oth0@users.noreply.github.com>
2022-09-05 22:38:22 +0300

    ProjectTitan55%DONE

--- ProjectTitan/Assets/PlayerManager.cs
+++ ProjectTitan/Assets/PlayerManager.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    public static int health = 5;
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.transform.tag == "Enemy")
-        {
-            
-        }
-    }
+    
+        
 }
